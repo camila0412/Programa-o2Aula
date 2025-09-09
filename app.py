093501f4b56386acf89d02fd224bf1406030fe04
camila0412 <camila.warmeling04@gmail.com>
2025-09-09 09:23:06 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+import mysql.connector
+
+
+app = Flask(__name__)
+
+app.secret_key = 'baincaCaimla0412'
+
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '',
+    'database': 'login'
+}
+
+
+@app.route('/cadastro', methods = [ 'GET', 'POST'])
+def cadastro():
+    if request.method == 'POST':
+        nome = request.form['nome']
